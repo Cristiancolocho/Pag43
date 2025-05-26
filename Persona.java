03ade599230a6b51f3cc215249590c4eee640ae5
Cristian Daniel Colocho López <colochocristian@outlook.com>
2025-05-25 18:23:24 -0600

    modificando clase

--- Persona.java
+++ Persona.java
@@ -1,61 +1,59 @@
-public class Persona{
-private Integer Id;
-private String Nombre;
-private String Apellidos;
-private Integer Edad;
+public class Persona {
+    private Integer Id;
+    private String Nombre;
+    private String Apellidos;
+    private Integer Edad;
 
+    public Persona() {
+    }
 
-public Persona() {
-}
+    public Persona(Integer id, String nombre, String apellidos, Integer edad) {
+        Id = id;
+        Nombre = nombre;
+        Apellidos = apellidos;
+        Edad = edad;
+    }
 
+    public Integer getId() {
+        return Id;
+    }
 
-public Persona(Integer id, String nombre, String apellidos, Integer edad) {
-    Id = id;
-    Nombre = nombre;
-    Apellidos = apellidos;
-    Edad = edad;
-}
+    public void setId(Integer id) {
+        Id = id;
+    }
 
+    public String getNombre() {
+        return Nombre;
+    }
 
-public Integer getId() {
-    return Id;
-}
+    public void setNombre(String nombre) {
+        Nombre = nombre;
+    }
 
+    public String getApellidos() {
+        return Apellidos;
+    }
 
-public void setId(Integer id) {
-    Id = id;
-}
+    public void setApellidos(String apellidos) {
+        Apellidos = apellidos;
+    }
 
+    public Integer getEdad() {
+        return Edad;
+    }
 
-public String getNombre() {
-    return Nombre;
-}
+    public void setEdad(Integer edad) {
+        Edad = edad;
+    }
 
+    public void Concentrarse() {
+        System.out.println("Concentrandose");
 
-public void setNombre(String nombre) {
-    Nombre = nombre;
-}
+    }
 
+    public void Viajar() {
+        System.out.println("Viajando");
+    }
 
-public String getApellidos() {
-    return Apellidos;
-}
-
-
-public void setApellidos(String apellidos) {
-    Apellidos = apellidos;
-}
-
-
-public Integer getEdad() {
-    return Edad;
-}
-
-
-public void setEdad(Integer edad) {
-    Edad = edad;
-}
-
-         
-
+    
 }
