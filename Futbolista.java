0b012f6e85c66d9da3f6119af5fbd36b457424a6
Cristian Daniel Colocho López <colochocristian@outlook.com>
2025-05-25 18:27:27 -0600

    clase futbolista

---
+++ Futbolista.java
@@ -0,0 +1,40 @@
+public class Futbolista extends Persona {
+    private Integer dorsal;
+    private String demarcacion;
+
+    public Futbolista() {
+    }
+
+    public Futbolista(Integer id, String nombre, String apellidos, Integer edad, Integer dorsal, String demarcacion) {
+        super(id, nombre, apellidos, edad);
+        this.dorsal = dorsal;
+        this.demarcacion = demarcacion;
+    }
+
+    public Integer getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(Integer dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    public String getDemarcacion() {
+        return demarcacion;
+    }
+
+    public void setDemarcacion(String demarcacion) {
+        this.demarcacion = demarcacion;
+    }
+
+    @Override
+    public void Concentrarse() {
+        System.out.println("Concentrandose como futbolista");
+    }
+
+    @Override
+    public void Viajar() {
+        System.out.println("Viajando como futbolista");
+    }
+    
+}
